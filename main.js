8be47fb8040b951f73658d86ea0066c4dbe029a6
FraAccaBi <francescahuda@gmail.com>
2022-04-07 12:33:39 +0200

    backup commit

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
 /* Descrizione:
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+- document.querySelector() oppure getElementById()
+- div nell'html
+- var numbers = array 
+- Math.random | copio da w3s la funzione
+
  */
 
-let pop_numbers 
-let popArray = []
+//selzionare elemento della dom  dove inserire i numeri casuali
 
-for (let i = 0; i < 5; i++) {
-    pop_numbers = (Math.floor(Math.random() * 100))
-    popArray.push(pop_numbers)
-    document.querySelector('.popup').innerHTML = popArray
-    console.log(popArray)
-}
-
-setTimeout(function(){
-    document.querySelector('.popup').style.display = "none"  
-}, 6000)
+//inizializzo var dove salvare i numeri
+let randomElement = document.querySelector('.popup')
+//creare i numeri casuali 
 
 
-setTimeout(function(){
-    let answers =[]
+function generateNumbers(total_numbers, numbers_limit) {
+    let randomNumbers = []
 
-    for (let i = 0; i < popArray.length; i++) {
-        answers.push(Number(prompt(`inserisci il ${i} numero`)))
-        if (answers[i] === popArray[i]){
-            console.log(`corretto ${i + 1}/5`);
-        }     
+    while (randomNumbers.length !== total_numbers) {
+        const numbers = (Math.floor(Math.random() * numbers_limit))
+        if (!randomNumbers.includes(numbers)){
+            randomNumbers.push(numbers)
+        }      
     }
 
-    if (answers != popArray){
-        document.querySelector('.popup').innerHTML = 'complimenti, ottima memoria'
+     return randomNumbers
+} 
+//ora la funzione genererà 5 numeri random da 1 a 100
+const randomNumber = generateNumbers(5,100)
+
+console.log(randomNumber);
+
+randomElement.innerHTML = `memorizza questi numeri ${randomNumber}`
+/* //imposto il timer a 30secondi
+setTimeout(() => {
+    //l'untente deve inserire uno alla volta i numeri che ha visto precedentemente a schermo tramite prompt
+    //faccio un ciclo e chiedo all'utente di inserire i numeri
+    const user_try =[]
+    for (let i =0; i<5; i++){
+        let userNumber = Number(prompt('inserisci uno dei numeri che hai visto prima'))
+        console.log(userNumber);
+        //inserisci nell'array di tentaitvi solo se i numeri inseriti combaciano con l'array popup e se non sono già dentro l'array dei tentativi
+        if ((popArray.includes(userNumber)) && !user_try.includes(userNumber)){
+            user_try.push(userNumber)
+        }
     }
-}, 5000)
+    console.log(user_try); */
+    /* Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+   // alert(`hai indovinato ${user_try.length} / ${popArray.length}`)
+//},// 3000)
+
+//document.querySelector('.popup').style.display = "none"  
+
 
 
